1b0efd350e4aa87e7b880636be94c8faa6aa3828
seokahi <skh9797@naver.com>
2023-08-30 16:37:45 +0900

    feat: navbar 리스트 따로 생성

---
+++ src/resource/navbar.tsx
@@ -0,0 +1,14 @@
+export const navBarList = [
+    {
+        title:'About me',
+    },
+    {
+        title:'Skills',
+    },
+    {
+        title:'Archiving',
+    },
+    {
+        title:'Projects',
+    }
+]
