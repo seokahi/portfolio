8fd8e17e58fbccaa33ba3a74aa9527c827714ab6
seokahi <skh9797@naver.com>
2023-08-31 16:37:45 +0900

    feat: 메인페이지 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-
+import MainPage from './page/MainPage';
+import { Route, Routes,BrowserRouter} from 'react-router-dom';
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
