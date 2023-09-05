b58b14f368695d224c98cff70d2a8dfbfe9355a3
seokahi <skh9797@naver.com>
2023-09-05 16:37:45 +0900

    feat: 내 정보 담은 container 생성

---
+++ src/container/InfoContainer.tsx
@@ -0,0 +1,13 @@
+
+import MyInfoImg from "../component/info/MyInfoImg";
+
+
+
+export default function InfoContainer() {
+    return (
+        <>
+        <MyInfoImg/>
+        {/* <MyInfoText/> */}
+        </>
+    );
+}
