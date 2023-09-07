6bc4fb1eaba2d7b8c140845faa1892d888286a7e
seokahi <skh9797@naver.com>
2023-09-07 19:40:45 +0900

    feat: HomePage 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MainPage from './page/MainPage';
-import { Route, Routes,BrowserRouter} from 'react-router-dom';
+import HomePage from './page/HomePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Navigate to={'home'} />} />
         <Route path="/" element={<MainPage />}>
+          <Route path="home" element={<HomePage />} /> {/* HomePage 라우트 추가 */}
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
