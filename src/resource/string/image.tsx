e706cb98f1ee9f3c7c339502333e40ef082c0b2a
seokahi <skh9797@naver.com>
2024-01-01 21:59:45 +0900

    feat: skill 이미지 합친 image 파일 생성

---
+++ src/resource/string/image.tsx
@@ -0,0 +1,17 @@
+export const skillList = [
+    {
+        skill:'FrontEnd',
+        img: [
+            {img:"/src/assets/img/html-icon.png"},
+            {img:"/src/assets/img/css-icon.png"},
+            {img:"/src/assets/img/js-icon.png"}
+        ]
+    },
+    {
+        skill:'BackEnd',
+        img:[
+            {img:"/src/assets/img/ts-icon.png"}
+        ]
+    }
+
+]
