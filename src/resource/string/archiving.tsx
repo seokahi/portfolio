5ab21eeba06f272a2a5f92a5e328ef03d2eaef8a
seokahi <skh9797@naver.com>
2024-01-08 23:14:37 +0900

    feat: archiving 관련 정보 및 이미지 파일 생성

---
+++ src/resource/string/archiving.tsx
@@ -0,0 +1,26 @@
+import github from "../img/github-logo.png"
+export const ArchivingList = [
+    {
+        img:github,
+        archiving:'Github',
+        link:'https://github.com/seokahi',
+        title:'소스 코드 저장소입니다.',
+        describe: [
+            "과거 프로젝트. 프로그램 , 앱의 소스 코드",
+            "혼자서 코딩 연습을 위해 끄적이던 소스 코드",
+            "학부 시절 공부한 소스 코드"
+        ]
+    },
+    {
+        img:github,
+        archiving:'Github',
+        link:'https://github.com/seokahi',
+        title:'소스 코드 저장소입니다.',
+        describe: [
+            "과거 프로젝트. 프로그램 , 앱의 소스 코드",
+            "혼자서 코딩 연습을 위해 끄적이던 소스 코드",
+            "학부 시절 공부한 소스 코드"
+        ]
+    },
+   
+]
