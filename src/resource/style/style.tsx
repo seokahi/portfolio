1392350e7e155ae8c2e7045ec96e4d0f5a913ea9
seokahi <skh9797@naver.com>
2024-01-16 15:10:36 +0900

    style: 공통 스타일 추가

--- src/resource/style/style.tsx
+++ src/resource/style/style.tsx
@@ -1,23 +1,47 @@
 import { createGlobalStyle } from 'styled-components';
 
-
 const Style = createGlobalStyle`
-  @font-face {
-    font-family: 'GangwonEduPowerExtraBoldA';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/GangwonEduPowerExtraBoldA.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-      }
+  @import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap');
+
+
+  /* @font-face {
+      font-family: 'Rajdhani';
+      src: url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap');
+      font-weight: 400;
+      font-style: normal;
+  }
 
       @font-face {
-      font-family: 'GangwonEdu_OTFBoldA';
-      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/GangwonEdu_OTFBoldA.woff') format('woff');
-      font-weight: normal;
+        font-family: 'GangwonEduPowerExtraBoldA';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/GangwonEduPowerExtraBoldA.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+          }
+
+        @font-face {
+        font-family: 'GangwonEdu_OTFBoldA';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/GangwonEdu_OTFBoldA.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+        }
+
+        @font-face {
+      font-family: 'SejonghospitalBold';
+      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2312-1@1.1/SejonghospitalBold.woff2') format('woff2');
+      font-weight: 700;
       font-style: normal;
-      }
+} */
+@font-face {
+    font-family: 'PartialSansKR-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2307-1@1.1/PartialSansKR-Regular.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
 
   * {
-    font-family: 'GangwonEduPowerExtraBoldA', sans-serif;
+font-family: "Rajdhani", sans-serif;
+    font-weight: 400;
+    font-style: normal;
   }
 
   body {
