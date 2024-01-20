5dd4119dffb85c91a908d8744b8587d91d59f0ec
seokahi <skh9797@naver.com>
2024-01-20 17:10:36 +0900

    stlye: aboutme 스타일 변경

--- src/component/aboutme/AboutmeDetail.tsx
+++ src/component/aboutme/AboutmeDetail.tsx
@@ -2,22 +2,18 @@ import styled from 'styled-components';
 
 
 interface InfoType {
-  img: string;
-  info: string;
-  detail: string;
+    content:string;
 }
 
 interface AboutMeListProps {
   infos: InfoType; // props 이름을 infos로 변경
 }
  
-export const AboutMeList: React.FC<AboutMeListProps> = ({ infos }) => {
+export const AboutMeDetailList: React.FC<AboutMeListProps> = ({ infos }) => {
     return (
       <TitleTextWrapper>
-        <TitleShareImg src={infos.img} alt="share-icon"/>
         <div>
-        <TitleText>{infos.info}</TitleText>
-        <TitleText>{infos.detail}</TitleText>
+        <TitleText>{infos.content}</TitleText>
         </div>
       </TitleTextWrapper>
     );
@@ -25,25 +21,37 @@ export const AboutMeList: React.FC<AboutMeListProps> = ({ infos }) => {
 
   
 const TitleTextWrapper = styled.div`
-    display: flex;
+    /* display: flex;
     display: -ms-flexbox;
     flex-flow: row nowrap;
     width: 100%;
     max-width: 14rem;
     margin: 0 auto;
     opacity: .8;
+    border-radius: 8px;
+    box-shadow:rgba(89, 91, 92, 0.2) 0px 8px 24px;
+    margin-bottom: 3rem; */
+    display:flex;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: start;
+  justify-content: flex-start;
+
 `;
 
 const TitleText = styled.div`
-    margin-bottom: .5rem;
+    /* margin-bottom: .5rem;
     font-weight: 700;
-    font-size: 1.25rem;
+    font-size: 1.25rem; */
 `
 
 const TitleShareImg = styled.img`
- width: 2rem;
+ /* width: 2rem;
     min-width: 2rem;
     height: 2rem;
     margin-top: .2rem;
-    margin-right: 2rem;
+    margin-right: 2rem; */
 `
