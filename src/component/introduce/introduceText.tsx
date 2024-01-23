d2142cecf56af7e6f77df5e423d55bfe8e3183fc
seokahi <skh9797@naver.com>
2024-01-23 20:10:36 +0900

    feat: button 5초 뒤 생성 추가

--- src/component/introduce/introduceText.tsx
+++ src/component/introduce/introduceText.tsx
@@ -1,58 +1,55 @@
 
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { setCurrent } from '../../reducer/homeSlice';
 import { useDispatch } from 'react-redux';
-
+import { motion } from "framer-motion";
+import TextTypingAni from '../../hook/textAnimation';
+import { useEffect, useState } from 'react';
 
 
 export default function IntroduceText() {
     const dispatch = useDispatch();
-
+    const [showButton, setShowButton] = useState(false);
     const handleChildClick = () => {
         dispatch(setCurrent("About me"));
     };
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowButton(true);
+        }, 5000); // 5초 후에 버튼 표시
+        return () => clearTimeout(timer);
+    }, []);
+    
     return (
         <TextWrapper>
-                <h1>- 서가희 -</h1>
-                <h2>프론트엔드 개발자 포트폴리오</h2>
-            {/* <span>공부가 제일 쉬웠어요.</span>
-            <MoveButton>자세히 보기</MoveButton> */}
-            <MoveButton onClick={handleChildClick}>자세히 보기</MoveButton>
+                <TextTypingAni text={'KAHI \n PORTFOLIO'}/>
+                {showButton && <MoveButton onClick={handleChildClick}>저를 좀 더 알고싶으신가요?</MoveButton>}
         </TextWrapper>
     );
 }
 
-const TextWrapper = styled.div`
+
+
+const TextWrapper = styled(motion.div)`
     width: 750px;
     color: white;
-    margin:3rem auto;
     text-align: center;
-    h1 {
-        font-size: 3.5rem;
-        font-weight: 700;
-    }
-    h2 {
-        font-size: 2.5rem;
-        font-weight: 600;
-    }
+    /* padding-top: 20%; */
+    padding-left: 5.5rem;
+    box-sizing: border-box;
 `
 
 const MoveButton = styled.button`
-
-    background: rgba(133, 133, 133, 0.4);
-    border: none;
-    border-radius: 50%;
+    border-color: #ffffff;
+    border-width: 2px;
+    border-style: solid;
+    border-radius: 980px;
+    background-color: #000000;
+    height: 52px;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    cursor: pointer;
     color:white;
-    &::before,&::after {
-        content: '';
-        
-    background: #ffffff6b;
-    
-    margin: auto;
-    border-radius: 50%;
-    -webkit-animation: waveAnimation 3s infinite linear;
-    animation: waveAnimation 3s infinite linear;
-    }
 `
 
 
