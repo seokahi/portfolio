aabf6c8e14b6b9c14b0b5ffd4d0a8b4a74ffad10
seokahi <skh9797@naver.com>
2024-02-01 20:10:36 +0900

    remove: style 및 기능 수정

--- src/component/skill/skillList.tsx
+++ src/component/skill/skillList.tsx
@@ -1,91 +1,31 @@
-import styled, { keyframes } from 'styled-components';
+import styled from "styled-components";
+import { FRONTEND,BACKEND,ETC } from "../../resource/string/image";
 
 
-import { skillList } from '../../resource/string/image';
-import { useEffect, useRef, useState } from 'react';
-import { SkillListDetail } from './skillListDetail';
-
-
-
- 
-export const SkillList = () => {
-    const [isInViewport, setIsInViewport] = useState(false);
-  const ref = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    if (!ref.current) return; 
-    const callback = (entries: IntersectionObserverEntry[]) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setIsInViewport(true);
-        } else {
-          setIsInViewport(false);
-        }
-      });
-    };
-
-    const options = { root: null, rootMargin: "0px", threshold: 0 };
-
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(ref.current);
-
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
-
-
-    return (
-        <SkillLists className={isInViewport ? "frame-in" : ""} ref={ref}>
-            {skillList.map((info, index) => (
-                <SkillListDetail key={index} info={info} />
-            ))}
-        </SkillLists>
-    );
+export type SkillInfo = {
+  stack: string;
+  name: string;
+  value: number;
+  className: string;
+  content: string;
 };
 
-const frameInAnimation = keyframes`
- 0% {
-    transform : rotate(0deg)
-  }
 
-  /* 50% {
-    transform : rotate(180deg)
-  } */
+type SkillSet = [SkillInfo[]];
 
-  100% {
-    transform : rotate(360deg)
-  }
-`;
-
-  
-  const SkillLists = styled.div`
-  -webkit-box-sizing: border-box;
-  width: 100%;
-  max-width: 59rem;
-  padding: 4rem 2rem;
-  margin: 0 auto;
-  display: flex;
-  display: -ms-flexbox;
-  flex-flow: row wrap;
-  justify-content: space-between;
-  -webkit-box-pack: justify;
-  flex: 1 1 40%;
-  flex-direction: column;
-
-  display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-width: 100%;
-height: 100vh;
-
-&.frame-in {
-  animation: ${frameInAnimation} 2s forwards;
+const Skill = () => {
+  const skills: SkillSet[] = [[FRONTEND],[BACKEND],[ETC]];
+  return (
+    <SkillBox>
+      {skills.map((stack)=>{
+        
+      })}
+    </SkillBox>
+  )
 }
 
-`;
+const SkillBox = styled.div`
+  
+`
 
-
-
-
+export default Skill;
--- src/component/skill/skillListDetail.tsx
+++ src/component/skill/skillListDetail.tsx
@@ -1,51 +1,129 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
+
+import styled from "styled-components";
 
 
-interface InfoType {
-   skill: string;
-    img:{img:string}[];
-}
+// interface InfoType {
+//    skill: string;
+//     img:{img:string}[];
+// }
 
-interface SkillListProps {
-    info: InfoType;
-}
+// interface SkillListProps {
+//     info: InfoType;
+// }
  
-export const SkillListDetail: React.FC<SkillListProps> = ({ info }) => {
-    return (
-        <TitleTextWrapper>
-        <TitleText>{info.skill}</TitleText>
-        <SkillImages>
-            {info.img.map((image, index) => (
-                <TitleShareImg key={index} src={image.img} alt={`Skill Image ${index}`} />
-            ))}
-        </SkillImages>
-    </TitleTextWrapper>
-    );
-  };
+// export const SkillListDetail: React.FC<SkillListProps> = ({ info }) => {
+//     return (
+//         <TitleTextWrapper>
+//         <TitleText>{info.skill}</TitleText>
+//         <SkillImages>
+//             {info.img.map((image, index) => (
+//                 <TitleShareImg key={index} src={image.img} alt={`Skill Image ${index}`} />
+//             ))}
+//         </SkillImages>
+//     </TitleTextWrapper>
+//     );
+//   };
 
   
-const TitleTextWrapper = styled.div`
-    display: flex;
-    display: -ms-flexbox;
-    flex-flow: row nowrap;
-    width: 100%;
-    max-width: 14rem;
-    margin: 0 auto;
-    opacity: .8;
-    transform: perspective(2500px) rotateY(0);
-`;
+// const TitleTextWrapper = styled.div`
+//     display: flex;
+//     display: -ms-flexbox;
+//     flex-flow: row nowrap;
+//     width: 100%;
+//     max-width: 14rem;
+//     margin: 0 auto;
+//     opacity: .8;
+//     transform: perspective(2500px) rotateY(0);
+// `;
 
-const TitleText = styled.div`
+// const TitleText = styled.div`
 
-    /* 스타일 */
-`;
+//     /* 스타일 */
+// `;
 
-const SkillImages = styled.div`
-    /* 스타일 */
-`;
+// const SkillImages = styled.div`
+//     /* 스타일 */
+// `;
 
-const TitleShareImg = styled.img`
-width: 30px;
-height: 30px;
-    /* 스타일 */
-`;
+// const TitleShareImg = styled.img`
+// width: 30px;
+// height: 30px;
+//     /* 스타일 */
+// `;
+
+
+
+interface ProgressProps {
+  name?: string;
+  value: number;
+  open?: boolean;
+}
+
+const RADIUS = 54;
+const CIRCUMFERENCE = 2 * Math.PI * RADIUS;
+
+export const ProgressCircle = ({ value, name, open }: ProgressProps) => {
+
+  return (
+    <ProgressBox>
+      <div className='progress-wrap' >
+        <StyledProgress value={value} name={name} open={open}>
+          <defs>
+            <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+              <stop offset="0%" stop-color="#FFFEC4" />
+              <stop offset="50%" stop-color="#DFFFD8" />
+              <stop offset="90%" stop-color="#95DAC1" />
+            </linearGradient>
+          </defs>
+          <circle className='frame' cx={60} cy={60} r={RADIUS} strokeWidth="6" />
+          <circle className={`bar ${open ? 'animate' : ''}`} cx={60} cy={60} r={RADIUS} strokeWidth="6" />
+        </StyledProgress>
+        <strong className='text'>{name}</strong>
+      </div>
+    </ProgressBox>
+  )
+}
+
+const ProgressBox = styled.div`
+  position: relative;
+  
+  .progress-wrap {
+    width: 120px;
+    height: 120px;
+    transform: rotate(-90deg);
+  }
+  .text {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    text-align: center;
+    color: #3d3d3d;
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 120px;
+    transform: rotate(90deg);
+}
+`
+
+const StyledProgress = styled.svg<ProgressProps> `
+  width: 120px;
+  height: 120px;
+  .frame {
+    fill: white;
+    stroke: #f5f5f5b2;
+  }
+  .bar {
+    fill: transparent;
+    stroke: url(#gradient);
+    stroke-linecap: round;
+    stroke-dashoffset: ${({ name, value }) => name ? CIRCUMFERENCE * (1 - value / 100) : CIRCUMFERENCE};
+    stroke-dasharray: ${CIRCUMFERENCE};
+    transition: 1.8s ease-in-out;
+  }
+  .animate {
+    stroke-dashoffset: ${({ value }) => CIRCUMFERENCE * (1 - value / 100)};
+  }
+`
