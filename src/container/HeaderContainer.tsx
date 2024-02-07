b0ca6fab065420e05a672dd8ba8f2226b1aa6444
seokahi <skh9797@naver.com>
2024-02-07 13:10:23 +0900

    deploy: 배포준비

--- src/container/HeaderContainer.tsx
+++ src/container/HeaderContainer.tsx
@@ -1,9 +1,12 @@
-import Navbar from "../component/Navbar";
+import Navbar from "../component/home/Navbar";
+
+import InfoContainer from "./InfoContainer";
 
 
 export default function HeaderContainer() {
     return (
         <>
+        <InfoContainer/>
             <Navbar />
         </>
     );
