944ee86100e31e9ebc2205f90117fc7b9f6de937
seokahi <skh9797@naver.com>
2024-02-08 19:10:45 +0900

    style: navbar style 변경

--- src/component/common/Navbar.tsx
+++ src/component/common/Navbar.tsx
@@ -4,81 +4,56 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <>
-      <Wrapper>
+    <Header>
+      <NavWrapper>
         <NavLogo to={'/'}>Front-end Developer</NavLogo>
         <NavItems />
-      </Wrapper>
-    </>
+      </NavWrapper>
+    </Header>
   );
 }
 
-const Wrapper = styled.div`
-    /* position: fixed;
-    top: 0px;
-    left: 0px;
-    right: 0px;
-    padding: 1rem 1.5rem;
-    box-sizing: border-box;
-    display: flex;
-    font-weight: 500;
-    font-size: 30px;
-    z-index: 2000;
-    opacity: 1;
-    width: 86%;
-    margin:0 auto;
-    justify-content: space-between; */
+const Header= styled.div`
     position: fixed;
+    width: 100%;
+    height: 70px;
     top: 0px;
-    left: 0px;
-    right: 0px;
-    width: 100vw;
-    background: linear-gradient(90deg, #6F84E2 0%, #7BABE5 100%);
-    padding: 1rem 10.4167vw;
-    border-bottom-left-radius: 10vw;
-    border-bottom-right-radius: 10vw;
     box-sizing: border-box;
-    box-shadow: rgba(54, 113, 217, 0.25) 0px 4px 4px;
-    transition: top 0.5s ease 0s, opacity 0.5s ease 0s, visibility 0.5s ease 0s;
-    z-index: 2000;
-    opacity: 1;
+    z-index: 9999;
+
+
+`
+
+const NavWrapper = styled.div`
+    position: relative;
     display: flex;
-    font-size: 1.25rem;
-    font-weight: 600;
-
-    @media (max-width: 1140px) {
-      max-width: 67.25rem;
-    }
-
-    @media (max-width: 576px) {
-      flex-direction: column;
-    }
+    width: 100%;
+    height: 100%;
+    padding: 0 50px;
+    margin: 0 auto;
+    box-sizing: border-box;
+    justify-content: space-between;
+    color: white;
+    font-weight: 500;
+    z-index: 9999;
+      box-shadow: 0 0 5px rgba(0, 0, 0, 0.3), inset 0 0 1px rgba(255, 255, 255, 0.3);
+  color: rgba(0, 0, 0, 1);
 `;
 
 const NavLogo = styled(Link)`
-    /* border-top: 1px solid #000000;
-    width: 19.5%;
-    font-size: 1.6rem;
-    line-height: 1.2;
-    font-weight: 500;
-    color:black;
-    padding-top:1.2rem;
-    margin-bottom: 20px; */
     display: flex;
-    -webkit-box-align: center;
+    padding-left: 10px;
     align-items: center;
-    -webkit-box-flex: 1;
-    flex-grow: 1;
-    position: relative;
-    top: -5px;
     color:white;
-    @media (max-width: 1140px) {
+    font-size: 19px;
+    font-weight: 800;
+    /* @media (max-width: 1140px) {
       width: 100%;
       margin-bottom: 20px;
     }
 
     @media (max-width: 576px) {
       flex-direction: column;
-    }
+    } */
 
 `;
