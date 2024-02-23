ed3c2779b45c076e8f18e733eeb5952fb890ab95
seokahi <skh9797@naver.com>
2024-02-23 20:11:45 +0900

    feat: 프로젝트 클릭 시 상세 페이지 이동 hook 함수 추가

---
+++ src/hook/useMoveToDetail.tsx
@@ -0,0 +1,16 @@
+export const useMoveToSection = () => {
+
+    const handleMove = (sectionId: string) => {
+  
+      const sectionElement = document.getElementById(sectionId);
+      const sectionTop = sectionElement?.getBoundingClientRect().top as number;
+  
+      window.scrollTo({
+        top: window.scrollY + sectionTop,
+        behavior: 'smooth'
+      });
+  
+    };
+  
+    return { handleMove };
+  }
