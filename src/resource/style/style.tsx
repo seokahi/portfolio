2776bdb5b9f940e6ab6d3bf52ea5ec42a662e509
seokahi <skh9797@naver.com>
2024-02-27 22:15:45 +0900

    fix: 배포 에러 해결

--- src/resource/style/style.tsx
+++ src/resource/style/style.tsx
@@ -4,33 +4,6 @@ const Style = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap');
 
 
-  /* @font-face {
-      font-family: 'Rajdhani';
-      src: url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap');
-      font-weight: 400;
-      font-style: normal;
-  }
-
-      @font-face {
-        font-family: 'GangwonEduPowerExtraBoldA';
-        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/GangwonEduPowerExtraBoldA.woff') format('woff');
-        font-weight: normal;
-        font-style: normal;
-          }
-
-        @font-face {
-        font-family: 'GangwonEdu_OTFBoldA';
-        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/GangwonEdu_OTFBoldA.woff') format('woff');
-        font-weight: normal;
-        font-style: normal;
-        }
-
-        @font-face {
-      font-family: 'SejonghospitalBold';
-      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2312-1@1.1/SejonghospitalBold.woff2') format('woff2');
-      font-weight: 700;
-      font-style: normal;
-} */
 @font-face {
     font-family: 'PartialSansKR-Regular';
     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2307-1@1.1/PartialSansKR-Regular.woff2') format('woff2');
