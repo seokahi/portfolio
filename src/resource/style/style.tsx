5e791433dbcb5b4cf67f59d6f9267743da9cb976
seokahi <skh9797@naver.com>
2024-02-27 23:20:45 +0900

    fix: 배포 해결중

--- src/resource/style/style.tsx
+++ src/resource/style/style.tsx
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const Style = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap');
-
 
 @font-face {
     font-family: 'PartialSansKR-Regular';
